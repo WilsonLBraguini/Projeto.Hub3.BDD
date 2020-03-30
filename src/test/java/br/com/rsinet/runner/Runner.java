1553f1d5defcee9a418ca36aef3cb2548994d5f3
Wilson Lucchini Braguini <wilson.braguini@rsinet.com.br>
2020-03-30 16:23:51 -0300

    Criando ProjetoRsi.Hub3.BDD

---
+++ src/test/java/br/com/rsinet/runner/Runner.java
@@ -0,0 +1,22 @@
+package br.com.rsinet.runner;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features/cenario_website.feature",
+		glue = "br.com.rsinet.steps",
+		plugin = "pretty", monochrome = true,
+		snippets = SnippetType.CAMELCASE,
+        dryRun = false,
+        strict = false
+        
+)
+public class Runner {
+	
+
+
+}
---
+++ src/test/java/br/com/rsinet/steps/CadastroPositivoSteps.java
@@ -0,0 +1,31 @@
+package br.com.rsinet.steps;
+
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CadastroPositivoSteps {
+	
+	
+	@Given("^que apos preencher todos os campos$")
+	public void que_apos_preencher_todos_os_campos() throws Throwable {
+
+
+	}
+
+	@When("^clicar no botao register$")
+	public void clicar_no_botao_register() throws Throwable {
+		
+	}
+
+	@Then("^o cadastro deve ser realizado com sucesso$")
+	public void o_cadastro_deve_ser_realizado_com_sucesso() throws Throwable {
+	  
+	}
+	
+	
+
+
+}
